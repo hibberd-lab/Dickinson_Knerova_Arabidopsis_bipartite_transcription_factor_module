96dbaee0d029a00dfd59c630faec6bbc37077d08
PJDickinson <45768523+PJDickinson@users.noreply.github.com>
2020-06-11 15:00:01 +0100

    Update Figure_1A_GO_term_plot.R

--- Figure_1/Figure_1A_GO_term_plot.R
+++ Figure_1/Figure_1A_GO_term_plot.R
@@ -3,3 +3,5 @@ library(ggplot2)
 AtBS_top200_GO_top20 <- read.csv("AtBS_top200_GO_top20.csv")
 
 ggplot(AtBS_top200_GO_top20, aes(x = reorder(Term, Fold_enrichment), y=Fold_enrichment)) + geom_bar(stat="identity" , fill="grey", colour="black") + theme_bw() + coord_flip()
+
+# glycosinolate biosynthetic glycosinolate metabolic process and glycosinolate biosynthetic process removed from GO terms as look like duplicates of glucosinolate metabolic process and glucosinolate biosynthetic process
