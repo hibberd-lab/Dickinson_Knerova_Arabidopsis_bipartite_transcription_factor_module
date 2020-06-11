5e3b835d3313688ea30221539a50d589ae0cb82e
PJDickinson <45768523+PJDickinson@users.noreply.github.com>
2020-06-11 11:36:45 +0100

    Delete Figure_1B_GLS_cell_type_plot.R

--- Figure_1/Figure_1B_GLS_cell_type_plot.R
+++
@@ -1,5 +0,0 @@
-library(ggplot2)
-
-At_GLS_expression <- read.csv("At_GLS_expression.csv")
-
-ggplot(At_GLS_expression, aes(x = reorder(id, -log2), y=log2)) + geom_bar(stat="identity" , fill="lightblue", colour="black") + theme_bw() + theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
