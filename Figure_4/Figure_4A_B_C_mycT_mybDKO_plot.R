734bdb61f8436401189e1d928a720bc63956f269
PJDickinson <45768523+PJDickinson@users.noreply.github.com>
2020-06-12 14:12:36 +0100

    Delete Figure_4A_B_C_mycT_mybDKO_plot.R

--- Figure_4/Figure_4A_B_C_mycT_mybDKO_plot.R
+++
@@ -1,9 +0,0 @@
-library(ggplot2)
-
-mycT_vs_mybDKO <- read.csv("mycT_vs_mybDKO.csv")
-
-ggplot(mycT_vs_mybDKO, aes(x=mycT, y=mybDKO, colour=mycT_mybDKO_down)) + scale_colour_manual(values=c("black", "red")) + geom_point() + theme_bw() + xlim(-10, 10) + ylim(-10, 10)
-
-ggplot(mycT_vs_mybDKO, aes(x=mycT, y=mybDKO, colour=mycT_down)) + scale_colour_manual(values=c("black", "red")) + geom_point() + theme_bw() + xlim(-10, 10) + ylim(-10, 10)
-
-ggplot(mycT_vs_mybDKO, aes(x=mycT, y=mybDKO, colour=mybDKO_down)) + scale_colour_manual(values=c("black", "red")) + geom_point() + theme_bw() + xlim(-10, 10) + ylim(-10, 10)
